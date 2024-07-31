9270b5693ef5d7200d4337d77bb491ab1314734c
Harsh Agrawal <letmecod@gmail.com>
2024-07-31 18:51:50 +0530

    Update Navbar.jsx (Put logo in navbar )

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { RiTodoLine } from "react-icons/ri";
 
 function Navbar() {
 
   return (
     <>
-    <div className = "px-4 py-4">
+    <div className = "px-4 py-4 flex items-center">
+      <RiTodoLine className= "text-2xl text-yellow-500"/>
     <h1 className='text-lg font-semibold'>ToDoYourself</h1>
     </div>
     </>
