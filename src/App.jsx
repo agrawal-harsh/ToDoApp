bc26167b1bc181907e1b1405178371d713911f91
agrawal-harsh <letmecod@gmail.com>
2024-07-31 12:05:46 +0530

    Project started

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar';
+
+function App() {
+
+  return (
+    <>
+    <div className='flex flex-col m-0 p-0 '>
+    <Navbar />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Navbar() {
+
+  return (
+    <>
+    <div className = "px-4 py-4 border-2">
+    <h1 className='text-lg font-semibold'>ToDoYourself</h1>
+    </div>
+    </>
+  )
+}
+
+export default Navbar;
